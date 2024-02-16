1ff5eb353d3058d810a9bfe0a77a108076d75045
maysaraujo <maysaaraujo25@gmail.com>
2024-02-16 16:44:33 -0300

    Finish the BoardGame object

--- main.js
+++ main.js
@@ -3,10 +3,8 @@ import './src/styles/generic/reset.css';
 import './src/styles/elements/base.css';
 
 import BoardGame from './src/objects/BoardGame';
-import PlayerScore from './src/components/PlayerScore';
 
 const $root = document.querySelector('#root');
 const $htmlBoardGame = BoardGame(6);
-const $htmlPlayerScore = PlayerScore();
 
-$root.insertAdjacentHTML('beforeend', $htmlPlayerScore + $htmlBoardGame);
+$root.insertAdjacentHTML('beforeend', $htmlBoardGame);
--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -1,12 +1,15 @@
 import CardGame from '../../components/CardGame';
+import './style.css';
 
 function BoardGame(amountCards) {
   const $htmlCardGame = CardGame();
-  const $htmlBoardGame = $htmlCardGame.repeat(amountCards);
+  const $htmlContent = $htmlCardGame.repeat(amountCards);
 
-  console.log($htmlCardGame);
-
-  return $htmlBoardGame;
+  return `
+    <section class="board-game">
+      ${$htmlContent}
+    </section>
+  `;
 }
 
 export default BoardGame;
